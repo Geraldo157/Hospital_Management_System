c1d4907e2fdde2e9d23158b3a936ff301b062038
Geraldo157 <gsmecanica76@gmail.com>
2023-05-12 03:43:34 -0300

    Criando projeto

---
+++ include/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+
+    <script src="https://code.jquery.com/jquery-3.7.0.slim.js" integrity="sha256-7GO+jepT9gJe9LB4XFf8snVOjX3iYNb0FHYr5LI1N5c=" crossorigin="anonymous"></script>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.1/css/fontawesome.min.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-info bg-info">
+        <h5 class="text-white ms-2">Hospital Management System</h5>
+        <div class="ms-auto"></div>
+        <ul class="navbar-nav me-3">
+            <li class="nav-item"><a href="#" class="nav-link text-white">Admin</a></li>
+            <li class="nav-item"><a href="#" class="nav-link text-white">Doctor</a></li>
+            <li class="nav-item"><a href="#" class="nav-link text-white">Patient</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<?php
+include "include/header.php";
+?>
+<div class="container">
+    <div class="col-md-12">
+        <div class="row">
+
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+
